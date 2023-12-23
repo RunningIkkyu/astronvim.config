cb994ab7f9a6444e0501d7c37bcaecaab4926e36
ikkyu <389498623@qq.com>
2023-12-23 23:57:59 +0800

    ✨ (feat): Change Codeium complete key to c-f

--- plugins/codeium.lua
+++ plugins/codeium.lua
@@ -1,6 +1,10 @@
 return {
   {
     'Exafunction/codeium.vim',
-    event = 'BufEnter'
+    event = 'BufEnter',
+    config = function ()
+    -- Change '<C-f>' here to any keycode you like.
+      vim.keymap.set('i', '<C-f>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+    end
   }
 }
